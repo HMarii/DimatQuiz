5ac5df3f4c363728e916f35eaba228f30c6fde9e
HarkaiMartin <maromarak@gmail.com>
2021-10-07 22:56:38 +0200

    fixxx

--- wolfscript.js
+++ wolfscript.js
@@ -68,12 +68,12 @@ nextButton.addEventListener('click', () =>{
 function startGame() {
 	if(soundchck.checked) {
 		if(playerName.value != '') {
-		var name = wolfsounds[Math.floor(Math.random() * wolfsounds.length + 1)];
+		var name = wolfsounds[Math.floor(Math.random() * wolfsounds.length)];
 		name.rate = 0.5
 		window.speechSynthesis.speak(name);
 	}
 	else {
-		var name = wolfsounds[Math.floor(Math.random() * wolfsounds.length + 1)];
+		var name = wolfsounds[Math.floor(Math.random() * wolfsounds.length)];
 		name.rate = 0.5;
 		window.speechSynthesis.speak(name);
 	}
